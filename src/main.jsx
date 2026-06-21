ef1425075a0c2411db6fb4eccc67c2a3ee7d49ed
getsolvacom <getsolva.com@gmail.com>
2026-06-21 17:24:35 +0100

    feat: add Sentry error monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,14 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://d6153b9f9fe01dd4e83e24a200ecdbfc0b4511604278891088.ingest.de.sentry.io/4511604313555824",
+  integrations: [
+    Sentry.replayIntegration(),
+  ],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
